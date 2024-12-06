08a3db7cf95ad8e127efc88ac42ef41be8de3520
dattasaigit <siramdattasai11@gmail.com>
2024-12-06 23:09:25 +0530

    Updated

--- src/main/java/com/klef/jfsd/springboot/SpringBootRestApijpaProjectApplication.java
+++ src/main/java/com/klef/jfsd/springboot/SpringBootRestApijpaProjectApplication.java
@@ -8,7 +8,7 @@ public class SpringBootRestApijpaProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApijpaProjectApplication.class, args);
-		System.out.println("Spring Boot Rest API and JPA Project...!!!");
+		System.out.println("Spring Boot Project...!!!");
 	}
 
 }
